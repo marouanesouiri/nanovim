9ac6a141a7691d5ff16350de9ea2f81b09334ba3
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-22 03:33:29 +0100

    edit: change aphlaton to 5rdala

--- lua/plugins/plugins/lsp.lua
+++ lua/plugins/plugins/lsp.lua
@@ -16,7 +16,7 @@ return { -- LSP Configuration & Plugins
 		--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
 		--    function will be executed to configure the current buffer
 		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("aphlaton-lsp-attach", { clear = true }),
+			group = vim.api.nvim_create_augroup("5rdala-lsp-attach", { clear = true }),
 			callback = function(event)
 				-- we create a function that lets us more easily define mappings specific
 				-- for LSP related items. It sets the mode, buffer and description for us each time.
