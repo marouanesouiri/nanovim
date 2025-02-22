b23e6fc69816133a3a53f7739eb11c0cba496d7b
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-22 03:27:59 +0100

    edit: remove auto pairs and comment line plugins (mini.nvim support this)

--- lua/plugins/plugins/etc.lua
+++ lua/plugins/plugins/etc.lua
@@ -1,40 +1,15 @@
 return {
-
 	{ -- Detect tabstop and shiftwidth automatically
 		"tpope/vim-sleuth",
 	},
-
-	{ -- "gc" to comment visual regions/lines
-		"numToStr/Comment.nvim",
-		opts = {},
-	},
-
 	{ -- undo-tree.nvim
 		"mbbill/undotree",
 	},
-
-	{ -- auto pairs
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = function()
-			require("nvim-autopairs").setup({
-				disable_filetype = { "TelescopePrompt", "spectre_panel" },
-				disable_in_macro = true, -- disable when recording or executing a macro
-				disable_in_visualblock = false, -- disable when insert after visual block mode
-				disable_in_replace_mode = true,
-				ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
-				enable_moveright = true,
-				enable_afterquote = true, -- add bracket pairs after quote
-				enable_check_bracket_line = true, --- check bracket in same line
-				enable_bracket_in_quote = true, --
-				enable_abbr = false, -- trigger abbreviation
-				break_undo = true, -- switch for basic rule break undo sequence
-				check_ts = false,
-				map_cr = true,
-				map_bs = true, -- map the <BS> key
-				map_c_h = false, -- Map the <C-h> key to delete a pair
-				map_c_w = false, -- map <c-w> to delete a pair if possible
-			})
-		end,
-	},
+	-- lua/plugins/c3.lua
+    {
+        "wstucco/c3.nvim",
+        config = function()
+            require("c3")
+        end,
+    },
 }
