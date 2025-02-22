fb0a4a25d05efad10adc88cdeee85c92d095ad70
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-22 19:52:45 +0100

    edit: buttons url field is a func now

--- lua/plugins/plugins/discord.lua
+++ lua/plugins/plugins/discord.lua
@@ -15,10 +15,13 @@ return {
 			client = "neovim", -- vim, neovim, lunarvim, nvchad, astronvim or your application's client id
 			tooltip = "The only One True Text Editor", -- Text to display when hovering over the editor's image
 		},
+		idle = {
+			enabled = false -- Enable idle status detection
+		},
 		display = {
 			show_time = true, -- Display start timestamp
 			show_repository = true, -- Display 'View repository' button linked to repository url, if any
-			show_cursor_position = true, -- Display line and column number of cursor's position
+			show_cursor_position = false, -- Display line and column number of cursor's position
 			swap_fields = false, -- If enabled, workspace is displayed first
 			swap_icons = false, -- If enabled, editor is displayed on the main image
 			workspace_blacklist = {}, -- List of workspace names that will hide rich presence
@@ -29,30 +32,44 @@ return {
 			scope = "workspace", -- buffer or workspace
 		},
 		idle = {
-			enable = true, -- Enable idle status
+			enable = false, -- Enable idle status
 			show_status = true, -- Display idle status, disable to hide the rich presence on idle
 			timeout = 300000, -- Timeout in milliseconds after which the idle status is set, 0 to display immediately
 			disable_on_focus = false, -- Do not display idle status when neovim is focused
 			text = "Idle", -- Text to display when idle
 			tooltip = "💤", -- Text to display when hovering over the idle image
 		},
+		-- text = {
+		-- 	viewing = "Viewing {}", -- Text to display when viewing a readonly file
+		-- 	editing = "Editing {}", -- Text to display when editing a file
+		-- 	file_browser = "Browsing files", -- Text to display when browsing files (Empty string to disable)
+		-- 	plugin_manager = "Managing plugins in {}", -- Text to display when managing plugins (Empty string to disable)
+		-- 	lsp_manager = "Configuring LSP in {}", -- Text to display when managing LSP servers (Empty string to disable)
+		-- 	vcs = "Committing changes in {}", -- Text to display when using Git or Git-related plugin (Empty string to disable)
+		-- 	workspace = "In my shit", -- Text to display when in a workspace (Empty string to disable)
+		-- },
 		text = {
-			viewing = "Viewing {}", -- Text to display when viewing a readonly file
-			editing = "Editing {}", -- Text to display when editing a file
-			file_browser = "Browsing files in {}", -- Text to display when browsing files (Empty string to disable)
-			plugin_manager = "Managing plugins in {}", -- Text to display when managing plugins (Empty string to disable)
-			lsp_manager = "Configuring LSP in {}", -- Text to display when managing LSP servers (Empty string to disable)
-			vcs = "Committing changes in {}", -- Text to display when using Git or Git-related plugin (Empty string to disable)
-			workspace = "In {}", -- Text to display when in a workspace (Empty string to disable)
+		 	viewing = "Viewing My shit", -- Text to display when viewing a readonly file
+		 	editing = "Editing My shit", -- Text to display when editing a file
+		 	file_browser = "Browsing files", -- Text to display when browsing files (Empty string to disable)
+		 	plugin_manager = "Managing plugins in {}", -- Text to display when managing plugins (Empty string to disable)
+		 	lsp_manager = "Configuring LSP in {}", -- Text to display when managing LSP servers (Empty string to disable)
+		 	vcs = "Committing changes in {}", -- Text to display when using Git or Git-related plugin (Empty string to disable)
+		 	workspace = "My shitty workspace", -- Text to display when in a workspace (Empty string to disable)
 		},
 		buttons = {
 			{
 				label = "View Repository", -- Text displayed on the button
-				url = "git", -- URL where the button leads to ('git' = automatically fetch Git repository URL)
+				url = function(opts)
+					if not opts.is_idle then return opts.repo_url end
+				end
 			},
 			{
 				label = "View Config",
 				url = "https://github.com/aphlatonn/nanovim",
+				url = function(opts)
+					if not opts.is_idle then return opts.repo_url end
+				end
 			},
 		},
 		assets = nil, -- Custom file icons, see the wiki*
