244ee1c9a757354b1c26bd11bd5b8653c8e523c3
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-22 22:48:22 +0100

    add keybinds to navigate splits using arrow keys

--- lua/settings/keybinds.lua
+++ lua/settings/keybinds.lua
@@ -62,6 +62,11 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- arrows version (some of my friends use my config and use the arrows)
+vim.keymap.set("n", "<C-Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-Right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-Down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-Up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- todo comments keymaps
 local todo = require("todo-comments")
