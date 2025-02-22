a9a28750a74003a75799c4c4b7873e221ff55a28
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-22 03:23:13 +0100

    add mini.nvim plugin

---
+++ lua/plugins/plugins/mini.lua
@@ -0,0 +1,10 @@
+return {
+	'echasnovski/mini.nvim',
+	version = '*',
+	config = function()
+		require('mini.surround').setup()
+		require('mini.pairs').setup()
+		require('mini.comment').setup()
+		require('mini.statusline').setup()
+	end
+}
