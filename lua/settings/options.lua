66b6aa386ab7705db8bfecea6270fb686e867542
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-22 21:10:10 +0100

    edit: move statusline.lua logic to options.lua file

--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -95,4 +95,16 @@ vim.opt.termguicolors = true
 -- remove the commands input place
 -- vim.opt.cmdheight = 0
 
+-- set vim.env.TERM to xterm-265color
 vim.env.TERM = "xterm-256color"
+
+-- custom status line
+-- " %f", -- File name
+-- " %{mode()}", -- Current mode
+-- " %r", -- Readonly flag
+-- " %m", -- Modified flag
+-- " %=", -- separator between left and right aligned items
+-- " %{&filetype}", -- Filetype
+-- ", %2p%%", -- Percentage through file
+-- ", %3l:%-2c ", -- Line and column
+-- vim.o.statusline = " %f %r %m %= --%{mode()}-- %{&filetype}, %2p%%,%3l:%-2c "
