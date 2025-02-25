186cb0064028a1f5bb344690dd12a2aa9dccb217
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-02-25 01:27:02 +0000

    edit: move lsp keybinds to keybinds file, and use the k func for remaps

--- lua/settings/keybinds.lua
+++ lua/settings/keybinds.lua
@@ -1,77 +1,87 @@
+local tbuiltin = require("telescope.builtin")
+local tthemes = require("telescope.themes")
+local harpoon = require("harpoon")
+local todo = require("todo-comments")
+
+-- Helper function for keybinds
+local function k(mode, key, action, desc)
+  vim.keymap.set(mode, key, action, { desc = desc })
+end
+
 -- HEAD: Keybinds for all modes
 
--- Interact with system clipboard
-vim.keymap.set("", "<Leader>p", '"+p', { desc = "Paste from system clipboard" }) -- Paste
-vim.keymap.set("", "<Leader>y", '"+y', { desc = "Yank to system clipboard" }) -- Yank
--- Copy entire buffer to clipboard
-vim.keymap.set("", "<Leader>yu", 'gg0vG$"+y', { desc = "Copy the buffer to system clipboard" })
---  HEAD: Normal mode keybinds.
---  remove the highlighting from the search mode
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+-- Clipboard operations
+k("", "<Leader>p", '"+p', "Paste from System Clipboard")
+k("", "<Leader>y", '"+y', "Yank to System Clipboard")
+k("", "<Leader>yu", 'gg0vG$"+y', "Copy the Buffer to Clipboard")
 
--- Telescope
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>f?", "<CMD>Telescope help_tags<CR>", { desc = "Search Help" })
-vim.keymap.set("n", "<leader>fm", "<CMD>Telescope<CR>", { desc = "Search Select Telescope" })
-vim.keymap.set("n", "<leader>fw", "<CMD>Telescope grep_string<CR>", { desc = "Search current Word" })
-vim.keymap.set("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", { desc = "Search by Grep" })
-vim.keymap.set("n", "<leader>fd", "<CMD>Telescope diagnostics<CR>", { desc = "Search Diagnostics" })
-vim.keymap.set("n", "<leader>fr", "<CMD>Telescope resume<CR>", { desc = "Search Resume" })
-vim.keymap.set("n", "<leader>f.", "<CMD>Telescope oldfiles<CR>", { desc = "Search Recent Files" })
-vim.keymap.set("n", "<leader>fb", "<CMD>Telescope buffers<CR>", { desc = "Find existing buffers" })
-vim.keymap.set("n", "<leader>ff", "<CMD>Telescope find_files<CR>", { desc = "Search Files" })
-vim.keymap.set("n", "<Leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Telescope todo marks" })
-vim.keymap.set("n", "<leader>f/", function()
-  builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({ winblend = 10, previewer = false, }))
-end, { desc = "Fuzzily search in current buffer" })
-vim.keymap.set("n", "<leader>s/", function()
-  builtin.live_grep({ grep_open_files = true, prompt_title = "Live Grep in Open Files", })
-end, { desc = "Search in Opened Files" })
+-- HEAD: Normal mode keybinds
+
+-- Remove search highlighting
+k("n", "<Esc>", "<cmd>nohlsearch<CR>", "Remove Search Highlighting")
+-- Telescope keymaps
+k("n", "<leader>f?", "<CMD>Telescope help_tags<CR>", "Search Help")
+k("n", "<leader>fm", "<CMD>Telescope<CR>", "Select Telescope")
+k("n", "<leader>fw", "<CMD>Telescope grep_string<CR>", "Search Current Word")
+k("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", "Search by Grep")
+k("n", "<leader>fd", "<CMD>Telescope diagnostics<CR>", "Search Diagnostics")
+k("n", "<leader>fr", "<CMD>Telescope resume<CR>", "Resume Search")
+k("n", "<leader>f.", "<CMD>Telescope oldfiles<CR>", "Search Recent Files")
+k("n", "<leader>fb", "<CMD>Telescope buffers<CR>", "Find Buffers")
+k("n", "<leader>ff", "<CMD>Telescope find_files<CR>", "Search Files")
+k("n", "<Leader>ft", "<cmd>TodoTelescope<CR>", "Telescope Todo Marks")
+k("n", "<leader>f/", function()
+  tbuiltin.current_buffer_fuzzy_find(tthemes.get_dropdown({ winblend = 10, previewer = false }))
+end, "Fuzzily Search in Current Buffer")
+k("n", "<leader>s/", function()
+  tbuiltin.live_grep({ grep_open_files = true, prompt_title = "Live Grep in Open Files" })
+end, "Search in Open Files")
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous Diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next Diagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic Error messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic Quickfix list" })
+k("n", "[d", vim.diagnostic.goto_prev, "Go to Previous Diagnostic")
+k("n", "]d", vim.diagnostic.goto_next, "Go to Next Diagnostic")
+k("n", "<leader>e", vim.diagnostic.open_float, "Show Diagnostic Errors")
+k("n", "<leader>q", vim.diagnostic.setloclist, "Open Diagnostic Quickfix List")
 
--- Key mappings for cycling through quickfix list
-vim.keymap.set("n", "[q", "<cmd>cnext<CR>", { desc = "Go next on Quick fix list" })
-vim.keymap.set("n", "]q", "<cmd>cprev<CR>", { desc = "Go previous on Quick fix list" })
+-- Quickfix list cycling
+k("n", "[q", "<cmd>cnext<CR>", "Go to Next in Quickfix List")
+k("n", "]q", "<cmd>cprev<CR>", "Go to Previous in Quickfix List")
 
--- redo with shift + u
-vim.keymap.set("n", "<S-u>", "<C-r>", { desc = "Just redo" })
+-- Undo/Redo operations
+k("n", "<S-u>", "<C-r>", "Redo")
 
--- buffer prev/next key maps
-vim.keymap.set("n", "<leader>bb", "<cmd>bp<CR>", { desc = "Buffer Previous" })
-vim.keymap.set("n", "[b", "<cmd>bp<CR>", { desc = "Buffer Previous" })
-vim.keymap.set("n", "<leader>bn", "<cmd>bn<CR>", { desc = "Buffer Next" })
-vim.keymap.set("n", "]b", "<cmd>bn<CR>", { desc = "Buffer Next" })
-vim.keymap.set("n", "<leader>br", "<cmd>b#<CR>", { desc = "Buffer Recent" })
-vim.keymap.set("n", "t", "<cmd>b#<CR>", { desc = "Buffer Recent" })
+-- Buffer navigation
+k("n", "<leader>bb", "<cmd>bp<CR>", "Previous Buffer")
+k("n", "<leader>bn", "<cmd>bn<CR>", "Next Buffer")
+k("n", "<leader>br", "<cmd>b#<CR>", "Recent Buffer")
+k("n", "[b", "<cmd>bp<CR>", "Previous Buffer")
+k("n", "]b", "<cmd>bn<CR>", "Next Buffer")
+k("n", "t", "<cmd>b#<CR>", "Recent Buffer")
 
--- undo tree
-vim.keymap.set("n", "<leader>tu", "<cmd>UndotreeToggle<CR>", { desc = "Toggle Undotree" })
+-- Undo Tree toggle
+k("n", "<leader>tu", "<cmd>UndotreeToggle<CR>", "Toggle Undo Tree")
 
--- copy buffer
-vim.keymap.set("n", "yu", "gg0vG$y", { desc = "Copy the buffer" })
+-- Copy buffer content
+k("n", "yu", "gg0vG$y", "Copy the Buffer")
 
--- Keybinds to make split navigation easier.
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
--- arrows version (some of my friends use my config and use the arrows)
-vim.keymap.set("n", "<C-Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-Right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-Down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-Up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- Window navigation keymaps
+k("n", "<C-h>", "<C-w><C-h>", "Move Focus Left")
+k("n", "<C-l>", "<C-w><C-l>", "Move Focus Right")
+k("n", "<C-j>", "<C-w><C-j>", "Move Focus Down")
+k("n", "<C-k>", "<C-w><C-k>", "Move Focus Up")
 
--- Bind Ctrl+w+r  to enter resize mode
-vim.keymap.set("n", "<C-w>r", function()
-  print("-- RESIZE --")
+-- Arrow key window navigation (for some users)
+k("n", "<C-Left>", "<C-w><C-h>", "Move Focus Left")
+k("n", "<C-Right>", "<C-w><C-l>", "Move Focus Right")
+k("n", "<C-Down>", "<C-w><C-j>", "Move Focus Down")
+k("n", "<C-Up>", "<C-w><C-k>", "Move Focus Up")
+
+-- Enter resize mode (Ctrl+w+r)
+k("n", "<C-w>r", function()
+  print("-- RESIZE MODE --")
   local opts = { noremap = true, silent = true, buffer = true }
 
-  -- Resize with h/j/k/l
+  -- Resize bindings
   vim.keymap.set("n", "h", ":vertical resize -1<CR>", opts)
   vim.keymap.set("n", "l", ":vertical resize +1<CR>", opts)
   vim.keymap.set("n", "k", ":resize +1<CR>", opts)
@@ -86,66 +96,68 @@ vim.keymap.set("n", "<C-w>r", function()
     vim.api.nvim_buf_del_keymap(0, "n", "j")
     vim.api.nvim_buf_del_keymap(0, "n", "<Esc>")
   end, opts)
-end, { desc = "Enter Resize Mode" })
+end, "Enter Resize Mode")
 
--- todo comments keymaps
-local todo = require("todo-comments")
-vim.keymap.set("n", "]t", function()
-  todo.jump_next()
-end, { desc = "Next todo comment" })
+-- Todo comments navigation
+k("n", "]t", function() todo.jump_next() end, "Next Todo Comment")
+k("n", "[t", function() todo.jump_prev() end, "Previous Todo Comment")
 
-vim.keymap.set("n", "[t", function()
-  todo.jump_prev()
-end, { desc = "Previous todo comment" })
-
-vim.keymap.set("n", "<leader>F", function()
+-- Format buffer using conform
+k("n", "<leader>F", function()
   require("conform").format({ async = true, lsp_fallback = true })
-end, { desc = "Format buffer" })
+end, "Format Buffer")
 
--- harpoon
-local harpoon = require("harpoon")
-vim.keymap.set("n", "<C-e>", function() harpoon:list():add()
-end, { desc = "Add to harpoon" })
+-- Harpoon keymaps
+k("n", "<C-e>", function() harpoon:list():add() end, "Add to Harpoon")
+k("n", "gh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, "Toggle Harpoon Menu")
+for i = 1, 9 do k("n", "g"..i, function() harpoon:list():select(i) end, "Select Harpoon "..i) end
 
-vim.keymap.set("n", "gh", function() harpoon.ui:toggle_quick_menu(harpoon:list())
-end, { desc = "Toggle harpoon menu" })
+-- Open compile mode
+k("n", "<leader>j", ":Compile ", "Open Compile Mode")
 
-for i = 1,9,1 do
-  vim.keymap.set("n", "g"..i, function() harpoon:list():select(1)
-  end, { desc = "Select harpoon "..i })
-end
+-- Search and replace word in buffer
+k("n", "<leader>cr", function()
+  local old_word = vim.fn.input("Word to replace: ")
+  if old_word == "" then return end
+  local new_word = vim.fn.input("Replace with: ")
+  local save_cursor = vim.fn.getpos(".")
+  vim.cmd("%s/\\<" .. old_word .. "\\>/" .. new_word .. "/g")
+  vim.fn.setpos(".", save_cursor)
+end, "Search and Replace in Buffer")
 
--- open compile mode
-vim.keymap.set("n", "<leader>j", ":Compile ", { desc = "Open compile mode" })
-
--- Replace the word under the cursor
-vim.keymap.set("n", "gr", function()
+-- Replace word under cursor
+k("n", "cc", function()
   local word = vim.fn.expand("<cword>")
-  local new_word = vim.fn.input("Replace with: ") 
+  local new_word = vim.fn.input("Replace with: ")
   if new_word ~= "" then
     local save_cursor = vim.fn.getpos(".")
     vim.cmd("%s/\\<" .. word .. "\\>/" .. new_word .. "/g")
     vim.fn.setpos(".", save_cursor)
   end
-end, { desc = "Replace word in buffer" })
+end, "Replace Word in Buffer")
 
--- Replace a word
-vim.keymap.set("n", "<leader>cr", function()
-  local old_word = vim.fn.input("Word to replace: ")
-  if old_word == "" then return end
+-- LSP keymaps
+k("n", "gd", tbuiltin.lsp_definitions, "Goto Definition")
+k("n", "gD", vim.lsp.buf.declaration, "Goto Declaration")
+k("n", "gr", tbuiltin.lsp_references, "Goto References")
+k("n", "gI", tbuiltin.lsp_implementations, "Goto Implementation")
+k("n", "<leader>lt", tbuiltin.lsp_type_definitions, "Type Definition")
+k("n", "<leader>ls", tbuiltin.lsp_document_symbols, "Document Symbols")
+k("n", "<leader>lp", tbuiltin.lsp_dynamic_workspace_symbols, "Workspace Symbols")
+k("n", "<leader>lr", vim.lsp.buf.rename, "Rename symbol")
+k("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
+k("n", "<leader>cd", tbuiltin.diagnostics, "Show Files with Errors")
+k("n", "<leader>ci", function()
+  vim.lsp.buf.code_action({
+    context = { only = { "source.addMissingImports" } }, apply = true,
+  })
+end, "Import Missing Items")
+k("n", "K", vim.lsp.buf.hover, "Hover Documentation")
 
-  local new_word = vim.fn.input("Replace with: ")
-  local save_cursor = vim.fn.getpos(".")
+-- HEAD: Visual mode keybinds
 
-  vim.cmd("%s/\\<" .. old_word .. "\\>/" .. new_word .. "/g")
-  vim.fn.setpos(".", save_cursor)
-end, { desc = "Search and replace in buffer" })
-
---  HEAD: Visual mode keybinds.
-
--- move the selected text up or down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected text down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text up" })
-vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selected text up" })
-vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selected text down" })
-
+-- Move selected text up or down
+k("v", "J", ":m '>+1<CR>gv=gv", "Move Selected Text Down")
+k("v", "K", ":m '<-2<CR>gv=gv", "Move Selected Text Up")
+k("v", "<S-Up>", ":m '<-2<CR>gv=gv", "Move Selected Text Up")
+k("v", "<S-Down>", ":m '>+1<CR>gv=gv", "Move Selected Text Down")
