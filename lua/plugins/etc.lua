b75d6fbe00910714b2a565ee6edca2c410796c65
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-05-11 13:29:45 +0100

    refactor: removimg c3.nvim plugin

--- lua/plugins/etc.lua
+++ lua/plugins/etc.lua
@@ -1,15 +1,6 @@
 return {
-	{ -- Detect tabstop and shiftwidth automatically
-		"tpope/vim-sleuth",
-	},
-	{ -- undo-tree.nvim
-		"mbbill/undotree",
-	},
-	-- lua/plugins/c3.lua
-    {
-        "wstucco/c3.nvim",
-        config = function()
-            require("c3")
-        end,
-    },
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
+	-- undo-tree.nvim
+	"mbbill/undotree",
 }
