0f03e091de22e4b26643272eb53af5170ec48933
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-06-01 07:39:17 +0100

    feat: add useful nvim options

--- init.lua
+++ init.lua
@@ -1,1 +1,18 @@
-print("Hello world")
+--{{ OPTIONS:
+vim.opt.mouse = "i"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.have_nerd_font = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
+vim.opt.scrolloff = 10
+vim.opt.laststatus = 3
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
+vim.opt.undofile = true
+vim.env.TERM = "xterm-256color"
+vim.opt.signcolumn = "yes"
+--}}
