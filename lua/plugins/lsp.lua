9255bf72adce406b4b26832b49581c1a17d51e9e
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-06-01 06:37:27 +0100

    refactor: clean up LSP config and use mason defaults

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,67 +1,82 @@
-return { 
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		-- Automatically install LSPs and related tools to stdpath for Neovim
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        {"mason-org/mason.nvim", opts = {}},
+        {"mason-org/mason-lspconfig.nvim", branch = "v1.x"},
+        {"j-hui/fidget.nvim", opts = {}},
+    },
+    opts = {
+        servers = {
+           lua_ls = {},
+           rust_analyzer = {},
+           clangd = {},
+           gopls = {},
+           ts_ls = {},
+        },
+    },
+    config = function(_, opts)
+        vim.api.nvim_create_autocmd('LspAttach', {
+          group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+            callback = function(event)
+                -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+                ---@param client vim.lsp.Client
+                ---@param method vim.lsp.protocol.Method
+                ---@param bufnr? integer some lsp support methods only in specific files
+                ---@return boolean
+                local function client_supports_method(client, method, bufnr)
+                    if vim.fn.has 'nvim-0.11' == 1 then
+                        return client:supports_method(method, bufnr)
+                    else
+                        return client.supports_method(method, { bufnr = bufnr })
+                    end
+                end
 
-		-- Useful status updates for LSP.
-		{ "j-hui/fidget.nvim", opts = {} },
-	},
-	opts ={ 
-		servers = {
-			lua_ls = {},
-			clangd = {},
-			gopls = {},
-			c3_lsp = {},
-		}
-	},
-	config = function(_, opts)
-		--  This function gets run when an LSP attaches to a particular buffer.
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("5rdala-lsp-attach", { clear = true }),
-			callback = function(event)
-				-- Highlight references of the word under cursor
-				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client.server_capabilities.documentHighlightProvider then
-					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-						buffer = event.buf,
-						callback = vim.lsp.buf.document_highlight,
-					})
+                -- The following two autocommands are used to highlight references of the
+                -- word under your cursor when your cursor rests there for a little while.
+                --    See `:help CursorHold` for information about when this is executed
+                --
+                -- When you move your cursor, the highlights will be cleared (the second autocommand).
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
+                if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+                    local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+                    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                        buffer = event.buf,
+                        group = highlight_augroup,
+                        callback = vim.lsp.buf.document_highlight,
+                    })
 
-					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-						buffer = event.buf,
-						callback = vim.lsp.buf.clear_references,
-					})
-				end
-			end,
-		})
+                    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                        buffer = event.buf,
+                        group = highlight_augroup,
+                        callback = vim.lsp.buf.clear_references,
+                    })
 
-		-- Create capabilities for LSP servers
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		-- Use blink.cmp instead of nvim-cmp for capabilities
-		capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+                    vim.api.nvim_create_autocmd('LspDetach', {
+                        group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+                        callback = function(event2)
+                            vim.lsp.buf.clear_references()
+                            vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                        end,
+                    })
+                end
+            end,
+        })
 
-		-- Setup mason
-		require("mason").setup()
+        -- Get capabilities for completion
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
-		-- Ensure these tools are installed
-		require("mason-tool-installer").setup({
-			ensure_installed = {
-				"gopls", "stylua", "clangd",
-			},
-		})
-
-		-- Setup mason-lspconfig with handlers
-		require("mason-lspconfig").setup({
-			handlers = {
-				function(server_name)
-					local server = opts.servers[server_name] or {}
-					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-					require("lspconfig")[server_name].setup(server)
-				end,
-			},
-		})
-	end,
+        -- Setup mason-lspconfig to auto-start servers
+        require("mason-lspconfig").setup({
+            ensure_installed = vim.tbl_keys(opts.servers or {}),
+            automatic_installation = false,
+            handlers = {
+                function(server_name)
+                    local server = opts.servers[server_name] or {}
+                    server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                    require("lspconfig")[server_name].setup(server)
+                end,
+            },
+        })
+    end,
 }
