dbfb4de9ddb4463da7788a9e03aa8ce10024be37
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-06-02 12:11:55 +0100

    feat: add neogit and undotree, and add keymaps to toggle them

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ vim.opt.laststatus = 3
 vim.opt.swapfile = false
 vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
 vim.opt.undofile = true
-vim.env.TERM = "xterm-256color"
+vim.env.term = "xterm-256color"
 vim.opt.signcolumn = "yes"
 vim.opt.path:append '**'
 vim.opt.tabstop = 4
@@ -33,15 +33,18 @@ if not vim.loop.fs_stat(mini_path) then
     'git', 'clone', '--filter=blob:none', '--depth=1',
     'https://github.com/echasnovski/mini.nvim', mini_path
   })
-  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('packadd mini.nvim | helptags all')
 end
 require('mini.deps').setup({ path = { package = path_package } })
 
--- Instalation
+-- instalation
 MiniDeps.add("nvim-treesitter/nvim-treesitter")
 MiniDeps.add("folke/snacks.nvim")
+MiniDeps.add("nvim-lua/plenary.nvim") -- required by neogit
+MiniDeps.add({ source = "neogitorg/neogit", checkout = "028e9ee"})
+MiniDeps.add("mbbill/undotree")
 
--- Configutation
+-- configutation
 require("mini.icons").setup()
 require("nvim-treesitter").setup({
     auto_install = true,
@@ -51,6 +54,7 @@ require("nvim-treesitter").setup({
 require("snacks").setup({
     picker = { enabled = true },
 })
+require("neogit").setup()
 --}}
 
 --{{ KEYMAPS:
@@ -62,28 +66,28 @@ end
 
 k("", "<leader>y", '"+y', "Yank to System Clipboard")
 k("", "<leader>p", '"+p', "Paste from System Clipboard")
-k("", "<leader>yu", 'gg0vG$"+y', "Copy the Buffer to Clipboard")
+k("", "yu", 'gg0vG$"+y', "Yank Buffer to System Clipboard")
 
 k("n", "<Esc>", "<cmd>nohlsearch<CR>", "Remove Search Highlighting")
 
 k("n", "-", "<cmd>Explore<CR>", "Open Netrw")
 
-k("n", "[q", "<cmd>cprev<CR>", "Go to Previous in Quickfix List")
-k("n", "]q", "<cmd>cnext<CR>", "Go to Next in Quickfix List")
-
 k("n", "<C-d>", "<C-d>zz", "Page down")
 k("n", "<C-u>", "<C-u>zz", "Page up")
-
-k("n", "<S-u>", "<C-r>", "Redo")
-
-k("n", "[b", "<cmd>bp<CR>", "Buffer Previous")
-k("n", "]b", "<cmd>bn<CR>", "Buffer Next")
-k("n", "<BS>", "<cmd>b#<CR>", "Buffer Recent")
 
 k("n", "<C-h>", "<C-w><C-h>", "Focus Left")
 k("n", "<C-l>", "<C-w><C-l>", "Focus Right")
 k("n", "<C-j>", "<C-w><C-j>", "Focus Down")
 k("n", "<C-k>", "<C-w><C-k>", "Focus Up")
+
+k("n", "<S-u>", "<C-r>", "Redo")
+
+k("n", "[q", "<cmd>cprev<CR>", "Go to Previous in Quickfix List")
+k("n", "]q", "<cmd>cnext<CR>", "Go to Next in Quickfix List")
+
+k("n", "[b", "<cmd>bp<CR>", "Buffer Previous")
+k("n", "]b", "<cmd>bn<CR>", "Buffer Next")
+k("n", "<BS>", "<cmd>b#<CR>", "Buffer Recent")
 
 k("n", "<leader><leader>", ":find ", "Find files")
 
@@ -92,9 +96,10 @@ k("n", "<leader>fb", require("snacks").picker.buffers, "Find buffer")
 k("n", "<leader>fg", require("snacks").picker.grep, "Find by grep")
 k("n", "<leader>fp", require("snacks").picker.projects, "Find project")
 
+k("n", "<leader>tg", "<cmd>Neogit<CR>", "Toggle neogit")
+k("n", "<leader>tu", "<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>", "Toggle undotree")
+
 k("v", "J", ":m '>+1<CR>gv=gv", "Move Selected Text Down")
 k("v", "K", ":m '<-2<CR>gv=gv", "Move Selected Text Up")
-k("v", "<S-Up>", ":m '<-2<CR>gv=gv", "Move Selected Text Up")
-k("v", "<S-Down>", ":m '>+1<CR>gv=gv", "Move Selected Text Down")
 --}}
 
