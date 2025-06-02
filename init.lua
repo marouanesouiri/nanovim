f5a73076af3eba9d6fa21836f5f6458e71afd8bb
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-06-02 12:19:16 +0100

    feat(autocmd): highlight text on yank

--- init.lua
+++ init.lua
@@ -57,6 +57,14 @@ require("snacks").setup({
 require("neogit").setup()
 --}}
 
+--{[ AUTOCOMMANDS:
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function() vim.highlight.on_yank() end,
+})
+--}}
+
 --{{ KEYMAPS:
 vim.g.mapleader = " "
 
