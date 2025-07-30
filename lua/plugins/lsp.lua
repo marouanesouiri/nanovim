2a980ec8ada4d6c522b22f6eab6fb79e53b0a826
Marouane Souiri <marwane.souiri.0@gmail.com>
2025-07-30 21:46:59 +0100

    feat: enable virtual text

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,82 +1,91 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        {"mason-org/mason.nvim", opts = {}},
-        {"mason-org/mason-lspconfig.nvim", branch = "v1.x"},
-        {"j-hui/fidget.nvim", opts = {}},
-    },
-    opts = {
-        servers = {
-           lua_ls = {},
-           rust_analyzer = {},
-           clangd = {},
-           gopls = {},
-           ts_ls = {},
-        },
-    },
-    config = function(_, opts)
-        vim.api.nvim_create_autocmd('LspAttach', {
-          group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
-            callback = function(event)
-                -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-                ---@param client vim.lsp.Client
-                ---@param method vim.lsp.protocol.Method
-                ---@param bufnr? integer some lsp support methods only in specific files
-                ---@return boolean
-                local function client_supports_method(client, method, bufnr)
-                    if vim.fn.has 'nvim-0.11' == 1 then
-                        return client:supports_method(method, bufnr)
-                    else
-                        return client.supports_method(method, { bufnr = bufnr })
-                    end
-                end
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		{ "mason-org/mason-lspconfig.nvim", branch = "v1.x" },
+		{ "j-hui/fidget.nvim", opts = {} },
+	},
+	opts = {
+		servers = {
+			lua_ls = {},
+			rust_analyzer = {},
+			clangd = {},
+			gopls = {},
+			ts_ls = {},
+		},
+	},
+	config = function(_, opts)
+		vim.diagnostic.config({ virtual_text = true })
 
-                -- The following two autocommands are used to highlight references of the
-                -- word under your cursor when your cursor rests there for a little while.
-                --    See `:help CursorHold` for information about when this is executed
-                --
-                -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                local client = vim.lsp.get_client_by_id(event.data.client_id)
-                if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-                    local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-                    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.document_highlight,
-                    })
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+			callback = function(event)
+				-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+				---@param client vim.lsp.Client
+				---@param method vim.lsp.protocol.Method
+				---@param bufnr? integer some lsp support methods only in specific files
+				---@return boolean
+				local function client_supports_method(client, method, bufnr)
+					if vim.fn.has("nvim-0.11") == 1 then
+						return client:supports_method(method, bufnr)
+					else
+						return client.supports_method(method, { bufnr = bufnr })
+					end
+				end
 
-                    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.clear_references,
-                    })
+				-- The following two autocommands are used to highlight references of the
+				-- word under your cursor when your cursor rests there for a little while.
+				--    See `:help CursorHold` for information about when this is executed
+				--
+				-- When you move your cursor, the highlights will be cleared (the second autocommand).
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if
+					client
+					and client_supports_method(
+						client,
+						vim.lsp.protocol.Methods.textDocument_documentHighlight,
+						event.buf
+					)
+				then
+					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.document_highlight,
+					})
 
-                    vim.api.nvim_create_autocmd('LspDetach', {
-                        group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-                        callback = function(event2)
-                            vim.lsp.buf.clear_references()
-                            vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-                        end,
-                    })
-                end
-            end,
-        })
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.clear_references,
+					})
 
-        -- Get capabilities for completion
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+					vim.api.nvim_create_autocmd("LspDetach", {
+						group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+						callback = function(event2)
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+						end,
+					})
+				end
+			end,
+		})
 
-        -- Setup mason-lspconfig to auto-start servers
-        require("mason-lspconfig").setup({
-            ensure_installed = vim.tbl_keys(opts.servers or {}),
-            automatic_installation = false,
-            handlers = {
-                function(server_name)
-                    local server = opts.servers[server_name] or {}
-                    server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-                    require("lspconfig")[server_name].setup(server)
-                end,
-            },
-        })
-    end,
+		-- Get capabilities for completion
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+
+		-- Setup mason-lspconfig to auto-start servers
+		require("mason-lspconfig").setup({
+			ensure_installed = vim.tbl_keys(opts.servers or {}),
+			automatic_installation = false,
+			handlers = {
+				function(server_name)
+					local server = opts.servers[server_name] or {}
+					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+					require("lspconfig")[server_name].setup(server)
+				end,
+			},
+		})
+	end,
 }
